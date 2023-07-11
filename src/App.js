f951efa1ff882866f1c282a791e7b35235732e25
AdrianaRoza <adriana.roza.aka@gmail.com>
2023-07-11 15:47:07 -0400

    feat: creating the element board

--- src/App.js
+++ src/App.js
@@ -28,7 +28,27 @@ export default function App() {
   const [currentSymbol,setCurrentSymbol] = useState('X')
   const [playing,setPlaying] = useState(true)
 
-  
+  const tabuleiro = (j) => {
+    return(
+      <div style={board}>
+        <div style={boardLine}>
+          <div style={house} data-pos='00' onClick="">{j[0][0]}</div>
+          <div style={house} data-pos='01' onClick="">{j[0][1]}</div>
+          <div style={house} data-pos='02' onClick="">{j[0][2]}</div>
+        </div>
+        <div style={boardLine}>
+          <div style={house} data-pos='10' onClick="">{j[1][0]}</div>
+          <div style={house} data-pos='11' onClick="">{j[1][1]}</div>
+          <div style={house} data-pos='12' onClick="">{j[1][2]}</div>
+        </div>
+        <div style={boardLine}>
+          <div style={house} data-pos='20' onClick="">{j[2][0]}</div>
+          <div style={house} data-pos='21' onClick="">{j[2][1]}</div>
+          <div style={house} data-pos='22' onClick="">{j[2][2]}</div>
+        </div>
+      </div>
+    )
+  }
 
   return(
     <>
